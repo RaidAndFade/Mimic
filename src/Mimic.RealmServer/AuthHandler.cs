a0e8b6c732b450496bdbf0494d899ed90c220508
FiniteReality <digpoealternate@gmail.com>
2018-03-08 01:22:18 +0000

    Implement first step of auth handshake for testing

--- src/Mimic.RealmServer/AuthHandler.cs
+++ src/Mimic.RealmServer/AuthHandler.cs
@@ -1,8 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Mimic.Common;
 
@@ -11,6 +17,10 @@ namespace Mimic.RealmServer
     internal class AuthHandler : ISocketHandler, IDisposable
     {
         private const uint GameName = 0x00_57_6f_57; // 'WoW'
+        private const string TestPassword = "TEST:Password";
+
+        private static readonly BigInteger N;
+        private static readonly BigInteger g;
 
         private bool _run = true;
         private TcpClient _client;
@@ -18,6 +28,15 @@ namespace Mimic.RealmServer
         private AsyncBinaryReader _reader;
 
         private AuthCommand _currentCommand;
+
+        static AuthHandler()
+        {
+            // TODO: pass these from a config file
+            N = BigInteger.Parse(
+"894B645E89E1535BBDAD5B8B290650530801B18EBFBF5E8FAB3C82872A3E9BB7",
+                NumberStyles.HexNumber);
+            g = new BigInteger(7);
+        }
 
         public async Task RunAsync(TcpClient client)
         {
@@ -38,6 +57,7 @@ namespace Mimic.RealmServer
                         break;
                     default:
                         Debug.WriteLine($"Unhandled opcode {cmd} (0x{cmd:X})");
+                        await CloseAsync(AuthError.Unimplemented);
                         break;
                 }
             }
@@ -87,18 +107,73 @@ namespace Mimic.RealmServer
             var accountNameLength = await reader.ReadUInt8Async();
             var accountName = await reader.ReadStringAsync(accountNameLength);
 
-            Debug.WriteLine("Read packet");
+            // TODO: clean up this code using a proper SRP6 handler
+
+            using (var sha = SHA1.Create())
+            {
+                // TODO: retrieve pre-hashed password information from db
+                var pw = Encoding.UTF8.GetBytes(TestPassword);
+                var hash = sha.ComputeHash(pw);
+
+                // Swap the endianness of the hash and pad with 0 to force
+                // a positive value (not needed in openssl)
+                Array.Reverse(hash);
+                Array.Resize(ref hash, hash.Length + 1);
+                hash[hash.Length - 1] = 0;
+
+                var x = new BigInteger(hash);
+
+                // Calculate server-side SRP values
+
+                var v = BigInteger.ModPow(g, x, N);
+                var s = BigInteger.Abs(new BigInteger(GetRandomBytes(32)));
+                var b = BigInteger.Abs(new BigInteger(GetRandomBytes(19)));
+                var gmod = BigInteger.ModPow(g, b, N);
+                var B = ((v * 3) + gmod) % N;
+
+                var unk3 = new BigInteger(GetRandomBytes(16));
+
+                List<byte> data = new List<byte>();
+
+                data.Add((byte)_currentCommand);
+                data.Add(0);
+                data.Add((byte)AuthError.Success);
+
+                // Pack data for client
+
+                // one-time key (32 bytes)
+                data.AddRange(B.ToByteArray().Take(32));
+                // generator (1 byte)
+                data.Add(1);
+                data.AddRange(g.ToByteArray().Take(1));
+                // safe prime (32 bytes)
+                data.Add(32);
+                data.AddRange(N.ToByteArray().Take(32));
+                // salt
+                data.AddRange(s.ToByteArray().Take(32));
+                // ???
+                data.AddRange(unk3.ToByteArray().Take(16));
+
+                // security flags
+                data.Add(0);
+
+                await _clientStream.WriteAsync(data.ToArray(), 0, data.Count);
+            }
         }
 
         private async Task SendErrorAsync(AuthError errorCode)
         {
+            // TODO: make sure this is a valid packet
+            // The client disconnects when we send this, which is good but not
+            // what we want.
             byte[] data = {
                 (byte)_currentCommand,
+                0x0,
                 0x0,
                 (byte)errorCode
             };
 
-            await _clientStream.WriteAsync(data, 0, 3);
+            await _clientStream.WriteAsync(data, 0, 4);
         }
 
         private async Task CloseAsync(AuthError errorCode)
@@ -107,6 +182,18 @@ namespace Mimic.RealmServer
             await Task.Delay(300); // Give the packet some time to be sent
             _client.Close();
             _run = false;
+        }
+
+        private byte[] GetRandomBytes(int count)
+        {
+            var result = new byte[count];
+
+            using (var random = RNGCryptoServiceProvider.Create())
+            {
+                random.GetBytes(result);
+            }
+
+            return result;
         }
     }
 }
--- src/Mimic.RealmServer/Enums/AuthError.cs
+++ src/Mimic.RealmServer/Enums/AuthError.cs
@@ -3,7 +3,7 @@ namespace Mimic.RealmServer
     public enum AuthError : byte
     {
         Success = 0x0,
-        ProtocolError = 0x1 // CMaNGOS: UNKNOWN0
-
+        ProtocolError = 0x1, // CMaNGOS: UNKNOWN0
+        Unimplemented = 0x2, // CMaNGOS: UNKNOWN2
     }
 }

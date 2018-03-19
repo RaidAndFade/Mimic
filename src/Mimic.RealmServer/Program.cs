dedae6d47c409863f617a1ee88c5747578694ca9
RaidAndFade <business@gocode.it>
2018-03-18 22:05:10 -0400

    use EFCORE

--- src/Mimic.RealmServer/Program.cs
+++ src/Mimic.RealmServer/Program.cs
@@ -1,25 +1,44 @@
 using Mimic.Common;
 using System;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace Mimic.RealmServer
 {
     class Program
     {
-        public static IDatabase authDatabase;
+        public static AuthDatabase authDatabase;
         
         static void Main(string[] args)
             => MainAsync(args).GetAwaiter().GetResult();
 
         static async Task MainAsync(string[] args)
         {
-            authDatabase = new MySQLDatabase(pass:"root",database:"mimic_auth");
-            authDatabase.createIfNonexistant();
+            string host = "127.0.0.1", database = "mimic_auth", user = "root", pass = "root";
+            var builder = new DbContextOptionsBuilder();
+            builder.UseMySQL($"Server={host}; database={database}; UID={user}; password={pass}");
+            authDatabase = new AuthDatabase(builder.Options);
+            authDatabase.Database.EnsureDeleted();
+            try{
+                authDatabase.Database.EnsureCreated();
+            }catch(MySql.Data.MySqlClient.MySqlException mse){
+                Debug.WriteLine(mse);
+            }
+            try
+            {
+                authDatabase.Accounts.Add(new AccountInfo { username = "RAID", pass_hash = "1848c99bb0beb9ccba402c3c1ca703095216c65d" });
+                authDatabase.SaveChanges();
+            }catch(Microsoft.EntityFrameworkCore.DbUpdateException due)
+            {
+                Debug.Write(due.GetBaseException());
+            }
+
             var socketManager = new SocketManager<AuthHandler>();
 
 
             await socketManager.StartAsync();
-            await authDatabase.init();
             await Task.Delay(-1);
         }
     }

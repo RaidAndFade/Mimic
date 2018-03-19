eb9976ceff02ceafdcf01ed4d8942a9fbb09eb3c
RaidAndFade <business@gocode.it>
2018-03-18 22:13:45 -0400

    OP Handler init. yummy

--- src/Mimic.WorldServer/Objects/Session/OpcodeHandle.cs
+++ src/Mimic.WorldServer/Objects/Session/OpcodeHandle.cs
@@ -1,7 +1,24 @@
+using System;
+using System.Reflection;
+
 namespace Mimic.WorldServer
 {
     public class OpcodeHandle
     {
-        
+        private MethodInfo _func;
+        private AuthStatus _authStatus;
+
+        public OpcodeHandle(AuthStatus authStatus, Action<WorldPacket> func){
+            _func = func.Method;
+            _authStatus = authStatus;
+        }
+
+
+        public void handle(AuthStatus curStatus, WorldSession ws, WorldPacket com){
+            if(curStatus < _authStatus){
+                throw new Exception("Auth status not good enough");
+            }
+            _func.Invoke(ws,new object[]{com});
+        }
     }
 }
--- src/Mimic.WorldServer/Objects/Session/OpcodeHandler.cs
+++ src/Mimic.WorldServer/Objects/Session/OpcodeHandler.cs
@@ -1,4 +1,5 @@
 using Mimic.WorldServer;
+using System.Diagnostics;
 
 namespace Mimic.WorldServer
 {
@@ -6,19 +7,31 @@ namespace Mimic.WorldServer
     {
         private WorldSession _session;
 
+        public static int NUM_OPCODES = 0x51f;
+
         private static OpcodeHandle[] _handles;
 
         public OpcodeHandler(WorldSession ws){
             _session = ws;
-            initOpcodes();
+            initOpcodes(ws);
         }
 
-        public void handleOpcode(){
-
+        public void handle(WorldPacket wp){
+            WorldCommand com = wp.cmd;
+            if(_handles[(int)com] == null){
+                Debug.Write("Received opcode "+com+" but it is not implemented yet. help!");
+                return;
+            }
+            _handles[(int)com].handle(_session._wh._status,_session,wp);
         }
 
-        public void initOpcodes(){
+        public static void initOpcodes(WorldSession ws){
+            if(_handles != null) return;
 
+            _handles = new OpcodeHandle[NUM_OPCODES];
+/* 0x037 */ _handles[(int)WorldCommand.CMSG_CHAR_ENUM] = new OpcodeHandle(AuthStatus.AUTHED,ws.RecvCharEnumRequest);
+/* 0x209 */ _handles[(int)WorldCommand.CMSG_READY_FOR_ACCOUNT_DATA_TIMES] = new OpcodeHandle(AuthStatus.AUTHED, ws.RecvDataTimesRequest); 
+/* 0x38C */ _handles[(int)WorldCommand.CMSG_REALM_SPLIT] = new OpcodeHandle(AuthStatus.AUTHED, ws.RecvRealmSplitRequest);
         }
 
     }
